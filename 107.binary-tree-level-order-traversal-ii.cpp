77d059d77f2f92fbce415e8fceb2909e12c10bfc
yumin2002 <yumin@umich.edu>
2023-06-20 21:27:42 -0400

    06/20

--- 107.binary-tree-level-order-traversal-ii.cpp
+++ 107.binary-tree-level-order-traversal-ii.cpp
@@ -19,8 +19,51 @@
 class Solution {
 public:
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
-        
+        vector<vector<int>> res;
+        queue<TreeNode*> q;
+        if (!root) return res;
+        q.push(root);
+        while (!q.empty()) {
+            int currSize = q.size();
+            vector<int> level;
+            for (int i = 0; i < currSize; i++) {
+                TreeNode* curr = q.front();
+                q.pop();
+                level.push_back(curr->val);
+                if (curr->left) q.push(curr->left);
+                if (curr->right) q.push(curr->right);
+            }
+            res.push_back(level);
+        }
+        return vector<vector<int>> (res.rbegin(), res.rend());
+
     }
 };
 // @lc code=end
 
+// some java recursice solution
+// class Solution {
+//     List<List<Integer>> levels = new ArrayList<List<Integer>>();
+
+//     public void helper(TreeNode node, int level) {
+//         // start the current level
+//         if (levels.size() == level)
+//             levels.add(new ArrayList<Integer>());
+
+//          // append the current node value
+//          levels.get(level).add(node.val);
+
+//          // process child nodes for the next level
+//          if (node.left != null)
+//             helper(node.left, level + 1);
+//          if (node.right != null)
+//             helper(node.right, level + 1);
+//     }
+    
+//     public List<List<Integer>> levelOrderBottom(TreeNode root) {
+//         if (root == null) return levels;
+//         helper(root, 0);
+//         Collections.reverse(levels);
+//         return levels;
+//     }
+// }
